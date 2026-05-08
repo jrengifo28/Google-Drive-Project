faa0d7cbf37130c514d14ab5aca2e6550b84baad
Jhonathan Rengifo Castillo <jrengifo28@hotmail.com>
2026-05-08 13:50:06 +0000

    Linked git account

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="center">
       <h1 className="h1">
-        StoreIt - The only storage solution you need.
+        StoreIt - The only storage solution you need..
       </h1>
     </div>
   );
