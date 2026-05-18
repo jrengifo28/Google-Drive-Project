972a1e9d2908e17175821ad2e5b64a8f37aaba90
Jhonathan Rengifo Castillo <jrengifo28@hotmail.com>
2026-05-18 17:04:22 +0000

    Adding links to signIn and signup pages

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -8,7 +8,6 @@ import { Button } from "@/components/ui/button";
 import {
     Form,
     FormControl,
-    FormDescription,
     FormField,
     FormItem,
     FormLabel,
@@ -17,6 +16,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
@@ -26,6 +26,7 @@ type FormType = "sign-in" | "sign-up";
 
 const AuthForm = ({ type }: { type: FormType }) => {
   const [isLoading, setLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("");
 
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -100,13 +101,27 @@ const AuthForm = ({ type }: { type: FormType }) => {
             )}
           />
 
-        <Button type="submit" className="form-submit-button">
+        <Button 
+        type="submit" 
+        className="form-submit-button"
+        disabled={isLoading}>
         {type === "sign-in" ? "Sign In" : "Sign Up"}
 
         {isLoading && (
           <Image src="/assets/icons/loader.svg" alt="loader" width={24} height={24} className="ml-2 animate-spin" />
         )}
         </Button>
+
+        {errorMessage && <p className="error-message">*{errorMessage}</p>}
+        <div className="body-2 flex justify-center">
+          <p className="text-light-100">
+            {type === "sign-in" ? "Don´t have an account?" : "Already have an account?"}
+          </p>
+          <Link href={type === "sign-in" ? "/sign-up" : "/sign-in"} className="ml-1 font-medium text-brand">
+          {type === "sign-in" ? "Sign Up" : "Sign In"}
+          </Link>
+        </div>
+
       </form>
     </Form>
   </>
