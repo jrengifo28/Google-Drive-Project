79e7ef52f4c60d712c9b52079186c90c70cc8d09
Jhonathan Rengifo Castillo <jrengifo28@hotmail.com>
2026-05-18 15:42:49 +0000

    Creating Adding hook for SignIn and SignUp

---
+++ components/AuthForm.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
+
+type FormType = "sign-in" | "sign-up";
+
+const AuthForm = ({ type }: { type: FormType }) => {
+  // 1. Define form
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Submit handler
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  }
+
+
+  return (
+  <>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+
+        <h1 className="form-title">
+          {type === "sign-in" ? "Sign In" : "Sign Up"}
+        </h1>
+
+        {type === "sign-up" && (
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <div className="shad-form-item">
+                  <FormLabel className="shad-form-label">
+                    Full name
+                  </FormLabel>
+
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your full name"
+                      className="shad-input"
+                      {...field}
+                    />
+                  </FormControl>
+                </div>
+
+                <FormMessage className="shad-form-message" />
+              </FormItem>
+            )}
+          />
+        )}
+
+        <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <div className="shad-form-item">
+                  <FormLabel className="shad-form-label">
+                    Email
+                  </FormLabel>
+
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your email"
+                      className="shad-input"
+                      {...field}
+                    />
+                  </FormControl>
+                </div>
+
+                <FormMessage className="shad-form-message" />
+              </FormItem>
+            )}
+          />
+
+        <Button type="submit">Submit</Button>
+
+      </form>
+    </Form>
+  </>
+);
+}
+
+export default AuthForm;
