6b762d8e9793a5c7550dfc9e24acd57c8997ba5e
Jhonathan Rengifo Castillo <jrengifo28@hotmail.com>
2026-08-19 04:39:44 +0000

    Creating Layout components

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Button } from './ui/button'
+import Image from 'next/image'
+import Search from './Search'
+import FileUploader from './FileUploader'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header className='header'>
+        <Search />
+        <div className='header-wrapper'>
+            <FileUploader />
+            <form>
+                <Button type="submit" className="sign-out-button">
+                    <Image 
+                    src="/assets/icons/logout.svg" 
+                    alt="logo" 
+                    width={24}
+                    height={24}
+                    className='w-6'
+                    />
+                </Button>
+            </form>
+        </div>
+    </header>
   )
 }
 
