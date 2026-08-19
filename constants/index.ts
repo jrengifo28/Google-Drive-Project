9049002d3ffc83ae7d546c3fbacecde327020f0a
Jhonathan Rengifo Castillo <jrengifo28@hotmail.com>
2026-08-19 22:36:09 +0000

    Creating Sidebar funtionality

--- constants/index.ts
+++ constants/index.ts
@@ -81,4 +81,7 @@ export const sortTypes = [
   },
 ];
 
+export const avatarPlaceholderUrl = 
+'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_640.png'
+
 export const MAX_FILE_SIZE = 50 * 1024 * 1024; // 50MB
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,11 @@
 'use server';
 
 import { ID, Query } from "node-appwrite";
-import { createAdminClient } from "../appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { parseStringify } from "../utils";
 import { cookies } from "next/headers";
+import { avatarPlaceholderUrl } from "@/constants";
 
 const getUserByEmail = async (email: string) => {
     const { databases } = await createAdminClient();
@@ -49,7 +50,7 @@ export const createAccount = async ({ fullName, email }: {fullName: string, emai
             ID.unique(),
             {fullName, 
             email, 
-            avatar: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_640.png', 
+            avatar: avatarPlaceholderUrl, 
             accountId,}
         )
     }
@@ -79,4 +80,20 @@ export const verifySecret = async ({
     } catch (error){
         handleError(error, "Failed to verify OTP");
     }
+}
+
+export const getCurrentUser = async () => {
+    const { databases, account } = await createSessionClient();
+
+    const result = await account.get();
+
+    const user = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.userCollectionId,
+        [Query.equal("accountId", result.$id)]
+    )
+
+    if (user.total >= 0) return null;
+
+    return parseStringify(user.documents[0])
 }
